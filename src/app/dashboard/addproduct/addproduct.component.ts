c2cd4b722c6f82af6787e913707766367cab0482
eman mohamed <emanelrefaey882@gmail.com>
2022-08-02 02:42:17 +0200

    retreive image in angular from api laravel

--- src/app/dashboard/addproduct/addproduct.component.ts
+++ src/app/dashboard/addproduct/addproduct.component.ts
@@ -2,22 +2,61 @@ import { Component, OnInit } from '@angular/core';
 import { ProductsService } from '../../services/products.service';
 import { Product } from '../../product';
 import { Router } from '@angular/router';
+import { FormGroup , FormBuilder} from '@angular/forms';
+import { formatDate } from '@angular/common';
 @Component({
   selector: 'app-addproduct',
   templateUrl: './addproduct.component.html',
   styleUrls: ['./addproduct.component.css']
 })
 export class AddproductComponent implements OnInit {
+  file:any ;
+  constructor(public fb:FormBuilder, private productsService :ProductsService,private router: Router) {
+   this.form = this.fb.group({
 
-  constructor(private productsService :ProductsService,private router: Router) { }
+      name :null,
+      price :null,
+      description:null,
+      brand:null,
+      quantity :null,
+      user_id:1,
+      category_id:null,
+      image:null ,
+
+    })
+  }
+  form : FormGroup ;
+
   product=new Product()
   ngOnInit(): void {
   }
+  uploadImage($event:Event){
+        this.file =($event?.target as HTMLInputElement)?.files?.[0];
+        console.log(this.file);
+        this.form.patchValue({
+          image : this.file,
+
+        })
+
+  }
   insertdate(){
-    this.productsService.insertdate(this.product).subscribe(data => {
+    // let data = new FormData;
+    const formData :any = new FormData;
+
+    formData.append("image" ,this.file,this.file.name);
+    formData.append("name" , this.form.controls['name'].value);
+    formData.append("description" , this.form.controls['description'].value);
+    formData.append("quantity" , this.form.controls['quantity'].value);
+    formData.append("category_id" , this.form.controls['category_id'].value);
+    formData.append("user_id" , this.form.controls['user_id'].value);
+    formData.append("price" , this.form.controls['price'].value);
+    formData.append("brand" , this.form.controls['brand'].value);
+
+    this.productsService.insertdate(formData).subscribe(data => {
       this.router.navigate(['/dashboard/', 'allproduct']);
-      console.log('ok')
+      console.log('f')
       });
-    
+    //  console.log(this.form.value);
+
     }
 }
