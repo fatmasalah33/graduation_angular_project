d4d688199141c05266353a968379438bf19c523a
fatma3333eg <fatma3333eg@gmail.com>
2022-08-12 22:16:02 +0200

    edit 3

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { CatogeryService } from '../services/catogery.service';
+import { OffersService } from '../services/offers.service';
 
 @Component({
   selector: 'app-home',
@@ -8,16 +9,25 @@ import { CatogeryService } from '../services/catogery.service';
 })
 export class HomeComponent implements OnInit {
   categories : Array<any> = [];
-  constructor(private _CatogeryService:CatogeryService ) { }
+  productOffered: Array<any> = [];
+  pathimage:any="http://127.0.0.1:8000/public/image/";
+  constructor(private _CatogeryService:CatogeryService ,private _OffersService:OffersService ) { }
 
   ngOnInit(): void {
     this.getallcategories()
+    this.getOffersproduct()
   }
   getallcategories(){
     this._CatogeryService.getmainCategory().subscribe((data : any) => {
       this.categories =data.data.categories ;
       console.log(data.data.categories)
       });
+  }
+  getOffersproduct(){
+    this._OffersService.productOffered().subscribe((data : any)=>{
+      console.log(data)
+      this.productOffered=data
+    })
   }
  
 }
